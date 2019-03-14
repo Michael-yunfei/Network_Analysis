c63d752d01c3df3795c6e7da0e0ce912f53f8f0e
Michael-cuhk <michael.yunfei@gmail.com>
2019-03-14 23:21:21 +0100

    Put more code on bipartite graph

--- Network_Analysis.R
+++ Network_Analysis.R
@@ -135,6 +135,164 @@ combn(3,2)
 
 
 
+# bipartite graph example 
+
+m = matrix(c(1 , 0 , 0 , 0,
+        1 , 1 , 0 , 0 ,
+        1 , 1 , 1 , 0 ,
+        0 , 1 , 0 , 1 ,
+        0 , 0 , 1 , 0 ,
+        0 , 0 , 0 , 1), nrow = 6, byrow = T)
+
+bg <- graph.incidence(m) # indicence for ploting bipartite graph 
+plot(bg, layout = layout_as_bipartite) # put the layout 
+
+V(bg)$type  # it gives two sets with FALSE and TRUE
+
+# now change the color 
+
+V(bg)$color <- ifelse(V(bg)$type == FALSE, "#649EFC", "#E75854")
+plot(bg, layout = layout_as_bipartite, vertex.size = 22, 
+        vertex.label.cex=1.6, edge.width = 1.6)
+
+
+# plot with the specific size 
+
+# set working directory with copied path (command+option+c)
+setwd('/Users/Michael/Library/Mobile Documents/com~apple~CloudDocs/Economics/Network/Notes')
+png(filename = "figure31.png",width = 400, height = 360)
+plot(bg, layout = layout_as_bipartite, vertex.size = 22, 
+        vertex.label.cex=1.6, edge.width = 1.6,
+        main = "A Bipartite Graph")
+dev.off()
+
+# try pdf 
+pdf(file = "figure31.pdf",width = 4, height = 3.6)
+plot(bg, layout = layout_as_bipartite, vertex.size = 26, 
+        vertex.label.cex=1, edge.width = 1.6, 
+        main = "A Bipartite Graph")
+dev.off()
+
+# try to change margins
+pdf(file = "figure311.pdf",width = 4, height = 3.6)
+par(mar = c(1, 0, 2, 1))  # use par after pdf file 
+plot(bg, layout = layout_as_bipartite, vertex.size = 26, 
+        vertex.label.cex=1, edge.width = 1.6, 
+        main = "A Bipartite Graph")
+dev.off()
+
+# Bipartite projection 
+
+pro <- bipartite_projection(bg)
+pdf(file = "figure32.pdf", width = 5.8, height = 3.6)
+par(mfrow = c(1,2), mar = c(1, 1, 2, 1), cex.main = 1, font.main = 1)
+plot(pro$proj1, layout = layout_in_circle, main = "Blue projecion on Red")
+plot(pro$proj2, layout = layout_in_circle, main = "Red projection on Blue")
+dev.off()
+
+# don't use layout_on_grid that ofthen, as it gives wrong connection. 
+plot(pro$proj1, layout = layout_on_grid(pro$proj1, dim = 2))  
+
+# get adjacency matrix 
+
+get.adjacency(pro$proj1, sparse = F, attr = "weight")
+
+# project matrix 
+
+cr = matrix(c(1 , 0 , 0 , 0 ,
+        1 , 1 , 0 , 0 ,
+        1 , 1 , 1 , 0 ,
+        0 , 1 , 0 , 1 ,
+        0 , 0 , 1 , 0 ,
+        0 , 0 , 0 , 1 ), nrow = 6, byrow = T)
+cb = matrix(c(1 , 1 , 1 , 0 , 0 , 0 ,
+        0 , 1 , 1 , 1 , 0 , 0 ,
+        0 , 0 , 1 , 0 , 1 , 0 ,
+        0 , 0 , 0 , 1 , 0 , 1 ), nrow = 4, byrow = T)
+
+adjmatrx = cr%*%cb
+diag(adjmatrx) = 0 
+adjmatrx  # it is equal to the one through equation get.adjacency 
+
+prmatrix(adjmatrx, rowlab = rep("", 6), collab = rep("", 6)) # print matrix without indices
+
+# adjacency matrix for red project 
+get.adjacency(pro$proj2, sparse = F, attr = "weight")
+adjmatrx2 = cb%*%cr
+diag(adjmatrx2) = 0 
+adjmatrx2
+
+prmatrix(adjmatrx2, rowlab = rep("", 4), collab = rep("", 4))  # print out matrix 
+
+
+# row sums shows the importance or partication rate of each member 
+rowSums(adjmatrx)
+rowSums(adjmatrx2)
+
+
+# nested split graph, where igraph does not support this kind of analysis
+# need install 
+
+# check the degree first 
+degree(bg)
+plot(bg, layout = layout_in_circle) # different plot layout 
+
+# creat a stepwise matrix 
+
+swm = matrix(c(0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+               1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 
+               1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 
+               1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+               1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+               1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+               1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+               1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+               1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+               1, 1, 0, 0, 0, 0, 0, 0, 0, 0), nrow = 10, byrow = T)
+nwswm <- graph_from_adjacency_matrix(swm, mode = "undirected")
+V(nwswm)$color = "#8ABCDD"
+plot(nwswm, layout = layout_in_circle)
+
+bipgraph <- graph.incidence(swm)
+V(bipgraph)$color <- ifelse(V(bipgraph)$type == FALSE, "#78C8A5", "#E75854")
+plot(bipgraph, layout = layout_as_bipartite)
+
+# I will put more code on nested split graph later 
+
+## Measuring and Visualizing network 
+# install.packages("sand") # which includes igraphdata , statistical analysis network with R
+install.packages("igraphdata")
+library(igraphdata)
+data("UKfaculty")
+plot(UKfaculty, layout = layout_in_circle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
