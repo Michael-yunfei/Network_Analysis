518a23ce066fa26e5d9eaaa805f6724c97def34b
Michael-cuhk <michael.yunfei@gmail.com>
2019-05-21 01:01:05 +0200

    update the code

--- Code and Programming/PeerEffect_fun.R
+++ Code and Programming/PeerEffect_fun.R
@@ -1,37 +1,37 @@
-## This is the function code to my paper - Structural identification and estimation of 
-## peer effects using network data 
-## @ Fei (Michael) Wang 
-## Student ID: 942870 
+## This is the function code to my paper - Structural identification and estimation of
+## peer effects using network data
+## @ Fei (Michael) Wang
+## Student ID: 942870
 
-## this is the main function file I have written, 
-## in some cases, it relies on the packages you should load in Part 0. 
+## this is the main function file I have written,
+## in some cases, it relies on the packages you should load in Part 0.
 ## it includes the following 6 functions:
-        # datatolist - a function takes the full dataset as input 
+        # datatolist - a function takes the full dataset as input
                        # and then gives the list which includes all
-                       # adjacency matrix 
+                       # adjacency matrix
 
-        # datatomatrix - a function converent network relationship into 
-                         # adjacency matrix 
+        # datatomatrix - a function converent network relationship into
+                         # adjacency matrix
 
         # dir_to_undir - a function convert directed adjacency matrix into
                          # undirected adjacency matrix
 
-        # checkmatrix - a function that checks the final block matrix has empty or 
-                         # all zero matrix 
+        # checkmatrix - a function that checks the final block matrix has empty or
+                         # all zero matrix
 
-        # expit - a funciont that calcuates the probability from 
+        # expit - a funciont that calcuates the probability from
                   # exponential model log odds ratio
 
-        # netw2sls - a function that estimating the model with 2SLS method 
+        # netw2sls - a function that estimating the model with 2SLS method
 
 
 datatolist <- function(m) {
-        ## input m is a dataframe, this function 
-        ## output is a list which includes all matrix 
-        ## this function only works for this dataset, it's not general one 
-        kgpmatrix <- list()  # create a list to store all matrix 
+        ## input m is a dataframe, this function
+        ## output is a list which includes all matrix
+        ## this function only works for this dataset, it's not general one
+        kgpmatrix <- list()  # create a list to store all matrix
         ni <- 0  # global counter
-        schindt <- as.numeric(unique(m$school))  # get vector to indicating school ID 
+        schindt <- as.numeric(unique(m$school))  # get vector to indicating school ID
         for (i in 1:length(schindt)) {
                 schid <- schindt[i]
                 schdata <- dplyr::filter(m, school == schid)  # filter out each school
@@ -44,38 +44,38 @@ datatolist <- function(m) {
                         names(kgpmatrix)[ni] <- paste("sch",schid, "class",classnumber, sep = "")
                 } else {
                         ni = ni+1
-                        # do fist class 
+                        # do fist class
                         classdata1 <- dplyr::filter(schdata, class == 1)  # filter out each class
                         adjmatrix1 <- datatomatrix(classdata1)
                         kgpmatrix[[ni]] <- adjmatrix1
                         names(kgpmatrix)[ni] <- paste("sch",schid, "class1", sep = "")
-                        # do second class 
-                        ni = ni+1 # count twice 
+                        # do second class
+                        ni = ni+1 # count twice
                         classdata2 <- dplyr::filter(schdata, class == 2)
                         adjmatrix2 <- datatomatrix(classdata2)
                         kgpmatrix[[ni]] <- adjmatrix2
                         names(kgpmatrix)[ni] <- paste("sch",schid, "class2", sep = "")
                 }
                 # mare sure adjmatrix 1 and adjmatrix 2 can be stored globally
-                
+
         }
         return(kgpmatrix)
-        
+
 }
 
 
 datatomatrix <- function(d){
-        ## a function converent network relationship into adjacency matrix 
+        ## a function converent network relationship into adjacency matrix
         ## input d should be data.frame
-        ## we use full dataset 
+        ## we use full dataset
         if (is.data.frame(d)){
-                datadim <- dim(d)  # get the dimension 
+                datadim <- dim(d)  # get the dimension
                 studentid <- as.numeric(d$studentid)
                 adjmatrix <- matrix(0, length(studentid)+1, length(studentid)+1)  # create a matrix with length of studentid + 1
                 adjmatrix[1, 2:ncol(adjmatrix)] <- studentid  # assign student id into the fist row and column
                 adjmatrix[2:nrow(adjmatrix), 1] <- studentid
                 for (i in 2:dim(adjmatrix)[1]) {
-                        friends <- as.numeric(d[i-1,1:32])  # convert it to numerical 
+                        friends <- as.numeric(d[i-1,1:32])  # convert it to numerical
                         idtofriends <- match(d[i-1, 1:32], adjmatrix[1, ], nomatch = 0)  # match friends
                         for (j in 1:length(idtofriends)) {
                                 if (idtofriends[j] != 0){
@@ -93,24 +93,24 @@ datatomatrix <- function(d){
 
 
 dir_to_undir <- function(dg) {
-        ## input is directed adjacency matrix list 
-        ## output is a undirected adjacency matrix list 
+        ## input is directed adjacency matrix list
+        ## output is a undirected adjacency matrix list
         library(igraph)
-        kgpmatrix_undirt <- dg  # initialize undirected list by assign the directed list 
+        kgpmatrix_undirt <- dg  # initialize undirected list by assign the directed list
         for (i in 1:length(kgpmatrix_undirt)){
                 gr <- graph.adjacency(dg[[i]])  ## convert it to graph
-                grun <- as.undirected(gr, mode = "mutual")  # only mutual friendship is kept 
+                grun <- as.undirected(gr, mode = "mutual")  # only mutual friendship is kept
                 grun_matrix <- as.matrix(get.adjacency(grun, type = "both"))  # concert s4 class to matrix
-                kgpmatrix_undirt[[i]] <-unname(grun_matrix)  
-                # only change matirx (without attributes), not the name 
+                kgpmatrix_undirt[[i]] <-unname(grun_matrix)
+                # only change matirx (without attributes), not the name
         }
         return(kgpmatrix_undirt)
 }
 
 
 checkmatrix <- function(matlst) {
-        nz = 0 
-        zeromat <- c()  # store the indices 
+        nz = 0
+        zeromat <- c()  # store the indices
         for (i in 1:length(matlst)) {
                 if (length(matlst[[i]]) == 0) {
                         print(names(matlst[i]))
@@ -128,26 +128,26 @@ checkmatrix <- function(matlst) {
 expit = function(x) 1/(1+exp(-x))  # write a function for calculating probability
 
 
-# 2sls estimation function 
+# 2sls estimation function
 netw2sls <- function(dataset, y, G, X){
         ## input
-        # dataset: cleaned full dataset (data.frame) that is used to consctruct ublock and jblock 
-        # y: dependent variable vector, 
-        # G: adjacency matrix 
-        # X: covariate variables (data.frame), 
-        
+        # dataset: cleaned full dataset (data.frame) that is used to consctruct ublock and jblock
+        # y: dependent variable vector,
+        # G: adjacency matrix
+        # X: covariate variables (data.frame),
+
         ## output
-        # a table includes all estimations except for fixed effect alpha 
-        # residuals 
-        # fitted values 
-        # a vector inlucdes, fixed effect - alpha, standar error and t ratio 
-        
-        
+        # a table includes all estimations except for fixed effect alpha
+        # residuals
+        # fitted values
+        # a vector inlucdes, fixed effect - alpha, standar error and t ratio
+
+
         # construct block unit vector model  and diagonal J block
         matrixlist <- datatolist(dataset)
         firstmatrix_len <- dim(matrixlist[[1]])[1]
         ublock <- matrix(1, firstmatrix_len)  # initialize the unit block by using the first submatrix of kgpcleanmat
-        jblock <- diag(firstmatrix_len) - 1/firstmatrix_len*ublock%*%t(ublock)  # initialize the J block 
+        jblock <- diag(firstmatrix_len) - 1/firstmatrix_len*ublock%*%t(ublock)  # initialize the J block
         for (i in 2:length(matrixlist)){
                 matlen <- dim(matrixlist[[i]])[1]
                 m1 <- matrix(1,matlen)
@@ -160,12 +160,12 @@ netw2sls <- function(dataset, y, G, X){
         if (! all(round(testju, 6) == 0)){
                 print("Warning: wrong ublock and jblcok matrix")
         }
-        
-        #create variables 
+
+        #create variables
         A <- G
-        y <- data.matrix(y)  # convert it into matrix format 
-        X <- data.matrix(X) 
-        ay <- A%*%y 
+        y <- data.matrix(y)  # convert it into matrix format
+        X <- data.matrix(X)
+        ay <- A%*%y
         ax <- A%*%X
         a2x <- A%*%ax
         Z <- cbind(ay,X, ax)
@@ -174,19 +174,19 @@ netw2sls <- function(dataset, y, G, X){
                 au <- A%*%ublock
                 hsub <- cbind(X, ax, a2x, au)
                 H <- jblock %*% hsub
-                P <- H %*% solve(t(H)%*%H) %*% t(H) 
-                
-        # estimate coefficients 
+                P <- H %*% solve(t(H)%*%H) %*% t(H)
+
+        # estimate coefficients
         estimations <- solve(t(Z) %*% P %*% Z) %*% t(Z) %*% P %*% y
         colnames(estimations) <- "Estimate"
         fixeffect <- y - Z %*% estimations  # get fixed effects (or constant one)
-        alpha = mean(fixeffect)  # get fixed effects 
+        alpha = mean(fixeffect)  # get fixed effects
         alphatratio <- alpha/sd(fixeffect)
         coefull <-  rbind(alpha, estimations)
         zfull <- cbind(alpha * matrix(1, dim(y)[1]), Z)
-        resids <- y - zfull %*% coefull  # get residuals 
+        resids <- y - zfull %*% coefull  # get residuals
         df <- dim(X)[1] - dim(Z)[2]  # degree of freedom, omit alpha at this moment
-        
+
         cov <- t(resids) %*% resids/ df
         covmatrix <- cov[[1]] * solve(t(Z) %*% P %*% t(P)%*% Z)
         stderror <- sqrt(diag(covmatrix))
@@ -194,14 +194,14 @@ netw2sls <- function(dataset, y, G, X){
         tratio <- estimations/stderror
         colnames(tratio) <- "t ratio"
         results <- cbind(estimations, stderror, tratio)
-        
-        # return a list which includes the results, residuals, and fitted values 
-        list(results = round(results, 4), 
-                residuals = resids , 
+
+        # return a list which includes the results, residuals, and fitted values
+        list(results = round(results, 4),
+                residuals = resids ,
                 yhat = zfull %*% coefull,
                 alpha = round(c(alpha, sd(fixeffect), alphatratio), 4))
-        
+
 
 }
 
-
+# End of Code
--- Code and Programming/PeerEffect_simulation.R
+++ Code and Programming/PeerEffect_simulation.R
@@ -1,32 +1,32 @@
-## This is the companion code to my paper - Structural identification and estimation of 
-## peer effects using network data 
-## @ Fei (Michael) Wang 
-## Student ID: 942870 
+## This is the companion code to my paper - Structural identification and estimation of
+## peer effects using network data
+## @ Fei (Michael) Wang
+## Student ID: 942870
 
 
-## simulation on model 2:  2SLS 
+## simulation on model 2:  2SLS
 
-# it takes around 10 minutes to run the whole simuations 
+# it takes around 10 minutes to run the whole simuations
 
 # The simulation part will only use two models:
-# model (2) 2SLS G 
-# model (2) 2SLS Ghat 
+# model (2) 2SLS G
+# model (2) 2SLS Ghat
 
 # dependent varaible:
-        # simulaton will only focus on average gpa 
-# independent variable: 
-        # previous year gpa, continuous 
+        # simulaton will only focus on average gpa
+# independent variable:
+        # previous year gpa, continuous
         # gender (dummy )
-        # age continuous 
+        # age continuous
         # IQ
-        # prestige, continous 
+        # prestige, continous
         # mother education university or not  (dummy)
         # father education university or not (dummy)
-        # GX 
+        # GX
 
 ##################       Part 0      ###################
 ## load essential pakcages and dataset
-rm(list = ls())  # clear the working memoery 
+rm(list = ls())  # clear the working memoery
 # Please set the working directory which includes the dataset - data_network2.csv
 setwd("/Users/Michael/Library/Mobile Documents/com~apple~CloudDocs/Economics/Network/Code and Programming")
 
@@ -36,8 +36,8 @@ for (i in 1:length(pkgs)){
         if (pkgs[i] %in% row.names(installed.packages()) == F) {
                 install.packages(pkgs[i])
         }
-}  # check packages and intall all 
-lapply(pkgs, require, character.only = TRUE)  # load all packages    
+}  # check packages and intall all
+lapply(pkgs, require, character.only = TRUE)  # load all packages
 
 source("PeerEffect_fun.R")  # Load all functions, please read instruction on functions
 # Load all essential data, please read the instrcution on those datasets and matrix
@@ -50,105 +50,105 @@ load("kgpcleanmat_undirt.Rdata")
 
 ##################       Part I  Simulation    ###################
 
-# simulate 100 times 
+# simulate 100 times
 set.seed(56897)  # set seed to make sure the simulation can be replicated.
 
 
-# create one matrix to store all simulated values, one matirx to stroe the density of network 
+# create one matrix to store all simulated values, one matirx to stroe the density of network
 simresults <- matrix(0,400, 6)  # 400 (4 x 100) by 6, 4 coefficents will be selected
 simdensity <- matrix(0, 100, 4)  # 100 by 4, 2-for density, 2-for mean_shortest_distance
 
-n = 0 
+n = 0
 
 for (si in 1:100) {
-        n = n+1 
-        
-        ##  prepare the data: Y, G, X 
+        n = n+1
+
+        ##  prepare the data: Y, G, X
         samplenetwork <- sample(1:101, 50, replace = F)  # generate 50 random numbers from 101
-        # subnetworks; we will run 100 simulations 
-        
-        sublist <- kgpcleanmat_undirt[samplenetwork] # get sublist of all network 
-        
-        # do block 
-        G <- sublist[[1]]  # initial the block matrix by assigning the first one 
+        # subnetworks; we will run 100 simulations
+
+        sublist <- kgpcleanmat_undirt[samplenetwork] # get sublist of all network
+
+        # do block
+        G <- sublist[[1]]  # initial the block matrix by assigning the first one
         for (i in 2:length(sublist)) {
                 G <- adiag(G, sublist[[i]])  # use adiag function from magic package
-                
+
         }
 
-        # get corresponding dataset 
+        # get corresponding dataset
         listname <- names(sublist)
         schclassid <- matrix(0, length(listname), 2)
         for (i in 1:length(listname)){
                 chr <- as.numeric(str_extract_all(listname[i], "\\d+")[[1]])
                 schclassid[i, 1:2] <- chr
-                
+
         }
         cdataset <- data.frame()
         for (i in 1:length(listname)){
                 kgpclean %>%
                         filter(school == schclassid[i, 1]) %>%
-                        filter(class == schclassid[i, 2]) -> sc 
+                        filter(class == schclassid[i, 2]) -> sc
                 cdataset <- rbind(cdataset, sc)
-                
+
         }
-        
-      
+
+
         cdataset %>%
-                dplyr::select(sch_gpa_68, fem, age , iq , prestige, 
+                dplyr::select(sch_gpa_68, fem, age , iq , prestige,
                         edu_motherd2, edu_fatherd2) ->simX
 
-        # Mode (2) 2SLS G 
+        # Mode (2) 2SLS G
         msls2sim <- netw2sls(cdataset,cdataset$sch_gpa_10,G,simX)
-        
-        # extract the results 
+
+        # extract the results
         select_coe <- rbind(msls2sim$results[c(1, 2, 5), ],msls2sim$alpha)
-        
+
         # store the values
-        simresults[(4*(si-1)+1):(4*si), 1:3] <- select_coe  # be careful on the order of operator 
-       
-        
-        # Mode (2) 2SLS Ghat 
+        simresults[(4*(si-1)+1):(4*si), 1:3] <- select_coe  # be careful on the order of operator
+
+
+        # Mode (2) 2SLS Ghat
         simnet <- network(G, directed = F)
         simgraph <- graph.adjacency(G, mode = "undirected")
-        simdensity[si, 1] <- network.density(simnet)  # store the density 
+        simdensity[si, 1] <- network.density(simnet)  # store the density
         simdensity[si, 3] <- mean_distance(simgraph, directed = F, unconnected = T)
-        
+
         cdataset %>%
                 dplyr::select(fem, sch_gpa_68, age, prestige, iq) -> attr
-        
-        for (i in 1:dim(attr)[2]){  # first column is ID, so start it from 2 
+
+        for (i in 1:dim(attr)[2]){  # first column is ID, so start it from 2
                 set.vertex.attribute(simnet,names(attr)[i], attr[[i]])  # use [[]] to extract value
         }
-        
+
         # fit ergm model
         sim_ergmfit <- ergm(simnet ~ edges + nodematch("fem") +
-                        nodematch("sch_gpa_68") + nodematch("age") 
+                        nodematch("sch_gpa_68") + nodematch("age")
                 + nodematch("prestige")
                 + nodematch("iq"))
-        
+
         ghat_ergmsim <- simulate(sim_ergmfit, nsim = 30)
         indt <- sample(1:30, 1, replace=F)
-        simdensity[si, 2] <- network.density(ghat_ergmsim[[indt]])  # store the density 
-        
-        # get Ghat 
+        simdensity[si, 2] <- network.density(ghat_ergmsim[[indt]])  # store the density
+
+        # get Ghat
         Ghat <- as.matrix.network(ghat_ergmsim[[indt]], matrix.type = "adjacency")
-        
+
         simgraphhat <- graph.adjacency(Ghat, mode = "undirected")
         simdensity[si, 4] <- mean_distance(simgraphhat, directed = F, unconnected = T)
-        
-        # 2SLS estimation 
+
+        # 2SLS estimation
         msls2sim_ghat <- netw2sls(cdataset, cdataset$sch_gpa_10, Ghat, simX)
-        
-        # extract the results 
+
+        # extract the results
         select_coe_ghat <- rbind(msls2sim_ghat$results[c(1, 2, 5), ],msls2sim_ghat$alpha)
-        
+
         # store the values
         simresults[(4*(si-1)+1):(4*si), 4:6] <- select_coe_ghat
 }
 
 
-# end the simulation 
+# end the simulation
 
 ##################       Part II  Simulation Analysis    ###################
 
@@ -158,20 +158,20 @@ simresults <- as.data.frame(simresults)
 simdensity <- as.data.frame(simdensity)
 colnames(simresults) <- c("Estimation(G)", "Std.Error(G)", "t-value(G)",
         "Estimation(Ghat)", "Std.Error(Ghat)", "t-value(Ghat)")
-colnames(simdensity) <- c("Density(G)", "Density(Ghat)", "Mean_distance(G)", 
+colnames(simdensity) <- c("Density(G)", "Density(Ghat)", "Mean_distance(G)",
         "Mean_distance(Ghat)")
 
 # for the row of simrults are:
         # phi
         # previous gpa (68)
         # iq
-        # alpha 
+        # alpha
 
 coefnames <- rep(c("phi", "pre_gpa", "iq", "alpha"), dim(simdensity)[1])
 simresults$coefnames <- coefnames
 
 head(simresults)
-simresults <- simresults[, c(7, 1:6)]  # change the order of columns 
+simresults <- simresults[, c(7, 1:6)]  # change the order of columns
 head(simresults)
 tail(simresults)
 
@@ -179,17 +179,17 @@ tail(simresults)
 save(simresults, file = "simresults.Rdata")
 save(simdensity, file = "simdensity.Rdata")
 
-## Now calculate the rejection rate, use 1.645 as benche mark 
+## Now calculate the rejection rate, use 1.645 as benche mark
 
 simresults %>%
         mutate(Grj = if_else(abs(`t-value(G)`) >=1.645, 1, 0)) %>%
-        mutate(Ghatrj = if_else(abs(`t-value(Ghat)`) >= 1.645, 1, 0)) ->simresults2 
+        mutate(Ghatrj = if_else(abs(`t-value(Ghat)`) >= 1.645, 1, 0)) ->simresults2
 
 simresults2 %>%
-        group_by(coefnames) %>% 
+        group_by(coefnames) %>%
         summarise(Grej_rate = mean(Grj), Ghatrej_rate = mean(Ghatrj)) ->rejection_rate
 
-stargazer(as.matrix(rejection_rate))  # print it out as latex format 
+stargazer(as.matrix(rejection_rate))  # print it out as latex format
 
 
 ## plot all estimates based on degree and distance. , it gives figure 5.2
@@ -210,7 +210,7 @@ par(cex.main = 1, font.main = 1)
 hist(simdensity$`Mean_distance(G)`, xlim = c(2,9),breaks = 10,
         col = "lightblue", xlab = "Average Shortest Distance among all nodes",
         main = "Histogram of Network Average Shortest Distance from Simulation")
-hist(simdensity$`Mean_distance(Ghat)`, xlim = c(2,9), breaks = 20, 
+hist(simdensity$`Mean_distance(Ghat)`, xlim = c(2,9), breaks = 20,
         col = rgb(0,0,0.3,0.2), add = T)
 legend("topright", legend = c("Original adjacency matrix - G",
         "Estimated adjacency matrix - Ghat"), pch=15, bty = "n",y.intersp = 1.3,
@@ -218,17 +218,17 @@ legend("topright", legend = c("Original adjacency matrix - G",
 dev.off()
 
 
-## exam the power size with degree and distance  
+## exam the power size with degree and distance
 
 
 simresults %>%
-        dplyr::filter(coefnames == "phi") -> simphi 
+        dplyr::filter(coefnames == "phi") -> simphi
 
-# plot of phi, gives figure 5.1 
+# plot of phi, gives figure 5.1
 pdf(file = "sim_phi.pdf", width = 5.6, height = 3.8)
 par(cex.main = 1, font.main = 1)
 hist(simphi$`Estimation(G)`,  col = "#AED8E5", xlim = c(-0.1, 0.3),
-        xlab = "Estimations of Peer Effect", 
+        xlab = "Estimations of Peer Effect",
         main = "Histogram of Peer Effect Estimation from Simulation")
 hist(simphi$`Estimation(Ghat)`, col = rgb(0,0,0.3,0.2), xlim = c(-0.1, 0.3), add = T)
 legend(-0.1, 26, legend = c("Original adjacency matrix - G",
@@ -249,50 +249,31 @@ dev.off()
 simphiall <- cbind(simphi, simdensity)
 
 
-### correlation matrix analysis 
+### correlation matrix analysis
 
 cordataphi <- cor(simphiall[, c(4, 7, 8:11)])
 
-# corrplot(cordataphi, method = "pie")  # plot the correlation for fun, need corrplot package 
-# 
-        
+# corrplot(cordataphi, method = "pie")  # plot the correlation for fun, need corrplot package
+#
+
 tvaluecorphi <-  rcorr(as.matrix(simphiall[, c(4, 7, 8:11)]))
 
-stargazer(round(tvaluecorphi$r, 4))  # print out as table 
+stargazer(round(tvaluecorphi$r, 4))  # print out as table
 
-stargazer(round(tvaluecorphi$P, 4)) 
+stargazer(round(tvaluecorphi$P, 4))
 
 
 simresults %>%
-        dplyr::filter(coefnames == "iq") -> simiq 
+        dplyr::filter(coefnames == "iq") -> simiq
 
 simiqall <- cbind(simiq, simdensity)
 
-# corrplot(cor(simiqall[, c(4, 7, 8:11)]))  # plot the correlation for fun, need corrplot package 
+# corrplot(cor(simiqall[, c(4, 7, 8:11)]))  # plot the correlation for fun, need corrplot package
 
 tvaluecoriq <-  rcorr(as.matrix(simiqall[, c(4, 7, 8:11)]))
 
-stargazer(round(tvaluecoriq$r, 4))  # print out as table 
+stargazer(round(tvaluecoriq$r, 4))  # print out as table
 
-stargazer(round(tvaluecoriq$P, 4)) 
+stargazer(round(tvaluecoriq$P, 4))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# End of Code
